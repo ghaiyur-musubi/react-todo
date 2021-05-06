1ddcda61df3adbdbc0e3963ee55e2aec15e3a8d6
Ghaiyur Naqui Ahmad <67789350+ghaiyur-musubi@users.noreply.github.com>
2021-05-06 23:26:45 +0530

    Check retention

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-import React,{useState} from 'react';
+import React,{useState,useRef,useEffect} from 'react';
 import TodoList from './TodoList'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
-  const  [todos, setTodos] = useState(['Todo 1','Todo 2'])
+  const [todos, setTodos] = useState([])
+  const todoNameRef = useRef()
+
+  const LOCAL_STORAGE_KEY = 'todosApp.todos'
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY,JSON.stringify(todos))
+  },[todos])
+
+  function toggleTodo(id) {
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id ===id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  function handleAddTodo(e){
+    const name=todoNameRef.current.value
+    if(name === "") return
+    setTodos(prevTodos =>{
+      return [...prevTodos,{id:uuidv4(),name:name,complete:false}]
+    })
+    todoNameRef.current.value = null
+
+  }
   return (
     <>
-      <TodoList todos={todos} />
-      <input type="text" />
-      <button>Add Todo</button>
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 todos left</div>
     </>
